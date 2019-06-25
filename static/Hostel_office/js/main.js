8da0bf7f6b2ab15a1ffbdf6a5c468d771ed03920
arjun <arjunmk368@gmail.com>
2019-06-25 15:32:58 +0530

    new

--- static/Hostel_office/js/main.js
+++ static/Hostel_office/js/main.js
@@ -285,3 +285,12 @@ function load_course() {
 
 load_dept_office();
 
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#minimal-tbody tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+
